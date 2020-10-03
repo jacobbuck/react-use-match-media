5cda27aa547ad7852615a363f1cb3dd32b66b790
Jacob Buck <buck.jacob@gmail.com>
2020-10-03 03:29:50 +0000

    Remove return in updateState function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const useMatchMedia = (mediaQueryString, initialState = false) => {
     if (window.matchMedia) {
       const mediaQueryList = window.matchMedia(mediaQueryString);
 
-      const updateState = () => setState(mediaQueryList.matches);
+      const updateState = () => {
+        setState(mediaQueryList.matches);
+      };
       updateState();
 
       mediaQueryList.addListener(updateState);
