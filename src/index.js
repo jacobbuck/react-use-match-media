4b652cde0703ce617c70440b07f8ecbee85caf86
Jacob Buck <buck.jacob@gmail.com>
2020-10-18 09:08:15 +0000

    Add type-checking (tests to come)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import { useState } from 'react';
 import useIsomorphicLayoutEffect from 'use-isomorphic-layout-effect';
 
 const useMatchMedia = (mediaQueryString, initialState = false) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (typeof mediaQueryString !== 'string') {
+      throw new TypeError(
+        `Expected \`mediaQueryString\` to be of type \`string\`, but received type \`${typeof mediaQueryString}\``
+      );
+    }
+    if (typeof initialState !== 'boolean') {
+      throw new TypeError(
+        `Expected \`initialState\` to be of type \`boolean\`, but received type \`${typeof initialState}\``
+      );
+    }
+  }
+
   const [state, setState] = useState(initialState);
 
   useIsomorphicLayoutEffect(() => {
